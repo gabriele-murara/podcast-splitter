3cf2ebd71a43f284df72e307a20310ee13a71d9a
Gabriele Murara <gabriele@murara.computer>
2026-03-14 10:34:59 +0100

    Closes #7 Implemented the pyproject.toml file and modified the version-returning function to read the version from the pyproject.toml file.

--- version.py
+++ version.py
@@ -1,8 +1,14 @@
-VERSION = "1.2.0-dev"
+import toml
+from pathlib import Path
+
 APP_NAME = "Podcast Splitter"
 
 def get_version():
-    return VERSION
+    # pyproject_file = Path(__file__).parent.parent / "pyproject.toml"
+    pyproject_file = Path(__file__).parent / "pyproject.toml"
+    pyproject = toml.load(pyproject_file)
+    return pyproject.get("project", {}).get("version", "0.0.0")
+
 
 def get_app_name():
     return APP_NAME
